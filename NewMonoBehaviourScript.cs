c0eeb934e49eb8366540863581bff55f21f93bb5
kjdhr <jbyyrh3@gmail.com>
2025-02-07 02:36:57 +0900

    1

---
+++ NewMonoBehaviourScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Studying()
+    {
+        Debug.Log("공부 너무 하기싫다");
+    }
+}
---
+++ NewMonoBehaviourScript1.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Studying()
+    {
+        Debug.Log("го╠Б ╫х╢ы");
+    }
+}
