d64d8e547f64c03a6d62a19d3ca522d863550337
kjdhr <jbyyrh3@gmail.com>
2025-02-07 02:40:21 +0900

    2

--- NewMonoBehaviourScript.cs
+++ NewMonoBehaviourScript.cs
@@ -5,6 +5,6 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Studying()
     {
-        Debug.Log("공부 너무 하기싫다");
+        Debug.Log("공부 진짜 하기싫다");
     }
 }
